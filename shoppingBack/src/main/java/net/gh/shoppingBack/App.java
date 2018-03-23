bc52649283b6666833870f9882600ca963fb4627
Anurag Ghosh <anuraghosh.1992@gmail.com>
2018-03-23 22:09:17 +0530

    done with initial settings

---
+++ shoppingBack/src/main/java/net/gh/shoppingBack/App.java
@@ -0,0 +1,13 @@
+package net.gh.shoppingBack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
